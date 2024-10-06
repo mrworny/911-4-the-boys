4b9dc50b64a758289ceb49663311e51434f688b9
bola <bolashotayo@gmail.com>
2024-10-06 11:06:22 +0100

    noting

--- app.js
+++ app.js
@@ -1,1 +1,75 @@
-console.log('hello');
+//console.log('Hello world');
+
+
+// data types : string,number,boolean.null,undefined
+
+const name = 'foo';
+const age = 30;
+const rating = 4.5;
+const iscool =true;
+const x = null;
+const y = undefined;
+let z;
+console. log(typeof )
+
+console. log('my name is ' + name + 'and i am ' + age);
+//template string 
+//console. log('my name is $(name) and i am $(age)');
+
+const s = 'hello world';
+
+let foo = 42; // foo is now a number
+foo = "bar"; // foo is now a string
+foo = true; // foo is now a boolean
+
+const foo = 42; // foo is a number
+const result = foo + "1"; // JavaScript coerces foo to a string, so it can be concatenated with the other operand
+console.log(result); // 421
+//false
+const bNoParam = Boolean();
+const bZero = Boolean(0);
+const bNull = Boolean(null);
+const bEmptyString = Boolean("");
+const bfalse = Boolean(false);
+//true
+const btrue = Boolean(true);
+const btrueString = Boolean("true");
+const bfalseString = Boolean("false");
+const bSuLin = Boolean("Su Lin");
+const bArrayProto = Boolean([]);
+const bObjProto = Boolean({});
+
+//Convert numeric strings and null to numbers
+
+Number("123"); // 123
+Number("123") === 123; // true
+Number("12.3"); // 12.3
+Number("12.00"); // 12
+Number("123e-1"); // 12.3
+Number(""); // 0
+Number(null); // 0
+Number("0x11"); // 17
+Number("0b11"); // 3
+Number("0o11"); // 9
+Number("foo"); // NaN
+Number("100a"); // NaN
+Number("-Infinity"); // -Infinity
+
+//Creating strings
+
+const string1 = "A string primitive";
+const string2 = 'Also a string primitive';
+const string3 = `Yet another string primitive`;
+
+//Comparing strings
+
+const a = "a";
+const b = "b";
+if (a < b) {
+  // true
+  console.log(`${a} is less than ${b}`);
+} else if (a > b) {
+  console.log(`${a} is greater than ${b}`);
+} else {
+  console.log(`${a} and ${b} are equal.`);
+}
